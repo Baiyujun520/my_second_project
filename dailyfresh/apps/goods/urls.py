0918d9529f1e7f75f456e65304799e32777bb1d3
白玉俊 <baiyujun010@163.com>
2017-12-15 21:02:48 +0800

    商品详情页面前后端页面实现

--- dailyfresh/apps/goods/urls.py
+++ dailyfresh/apps/goods/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from goods.views import IndexView
+from goods.views import IndexView, DetailView
 
 urlpatterns = [
-    url(r'^index$', IndexView.as_view(), name='index')
+    url(r'^index$', IndexView.as_view(), name='index'),  # 首页显示
+    url(r'^goods/(?P<sku_id>\d+)$', DetailView.as_view(), name='detail'),
 ]
--- dailyfresh/apps/goods/views.py
+++ dailyfresh/apps/goods/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
 from django.views.generic import View
 from goods.models import GoodsType, GoodsSKU, IndexGoodsBanner, IndexPromotionBanner, IndexTypeGoodsBanner
 from order.models import OrderGoods
@@ -33,3 +34,46 @@ class IndexView(View):
         }
 
         return render(request, 'index.html', context)
+
+
+class DetailView(View):
+    """商品详情视图"""
+    def get(self, request, sku_id):
+        """
+        1.根据前端传来的sku_id获取该商品的信息
+        2.获取商品的分类信息
+        3.获取商品的评论信息
+        4.获取新品信息
+        5.获取商品的其他规格
+        6.组织模板上下文
+        7.返回数据
+        :param request:
+        :return:
+        """
+        # 获取商品的信息
+        try:
+            sku = GoodsSKU.objects.get(id=sku_id)
+        except GoodsSKU.DoesNotExist:
+            return redirect(reverse('goods:index'))
+        # 获取商品分类信息
+        types = GoodsType.objects.all()
+        # 获取商品评论信息
+        sku_orders = OrderGoods.objects.filter(sku=sku).exclude(comment='')[:30]
+        # 获取新品信息
+        new_goods = GoodsSKU.objects.filter(type=sku.type).order_by('-create_time')[:2]
+
+        # 获取商品的其他规格
+        same_spu_skus = GoodsSKU.objects.filter(goods=sku.goods).exclude(id=sku_id)
+        cart_count = 0
+
+        context = {
+            'sku': sku,
+            'types': types,
+            'sku_orders': sku_orders,
+            'new_goods': new_goods,
+            'same_spu_skus': same_spu_skus,
+            'cart_count': cart_count
+        }
+
+        # 返回结果
+        return render(request, 'detail.html', context)
